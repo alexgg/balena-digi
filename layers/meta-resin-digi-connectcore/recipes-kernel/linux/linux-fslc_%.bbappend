alexgg/balena-digi-connectcore

Initial commit: Skeleton structure
According to the documentation at:
https://www.balena.io/os/docs/custom-build/#supporting-your-own-board

Signed-off-by: Alex Gonzalez <alex.gonzalez@digi.com>

layers/meta-resin-digi-connectcore/recipes-kernel/linux/linux-fslc_%.bbappend
@@ -1,0 +1,1 @@
+inherit kernel-balena

alexgg/balena-digi

layers/meta-resin-digi-connectcore/recipes-kernel/linux/linux-fslc_%.bbappend
@@ -1,0 +1,1 @@
+inherit kernel-resin